ab904b67b96e0a6d8666e4cc7e050867f6bb6fc3
MrLoh <flojoto@hotmail.com>
2018-09-05 16:49:43 +0200

    improve layout example

--- src/Layout.js
+++ src/Layout.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import posed, { PoseGroup } from 'react-pose';
-import { Link } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import styled from 'styled-components/native';
 
 const Wrapper = styled.View`
 	flex: 1;
-	align-items: center;
-	width: 100vw;
-	height: 100vh;
 	background-color: ${(p) => p.theme.colors.altBackground};
+	flex-direction: row;
 `;
 
 const SideBarWrapper = styled.View`
-	position: absolute;
 	background-color: ${(p) => p.theme.colors.secondary};
-	left: 0;
 	height: 100%;
+	width: 150px;
 	padding: 20px;
+`;
+
+const ContentWrapper = styled.View`
+	height: 100%;
+	flex: 1;
+	align-items: center;
+`;
+
+const Tiny = styled.Text`
+	margin-top: 20px;
+	font-size: 10px;
+	color: ${(p) => p.theme.colors.background};
 `;
 
 const Transition = posed.div({
@@ -27,12 +36,19 @@ const Transition = posed.div({
 const Layout = ({ children, location }) => (
 	<Wrapper>
 		<SideBarWrapper>
-			<Link to="/">Home -></Link>
-			<Link to="/about">About -></Link>
+			<NavLink exact to="/" activeStyle={{ fontWeight: 800 }}>
+				Home ->
+			</NavLink>
+			<NavLink to="/about" activeStyle={{ fontWeight: 800 }}>
+				About ->
+			</NavLink>
+			<Route path="/about" render={() => <Tiny>on about page</Tiny>} />
 		</SideBarWrapper>
-		<PoseGroup>
-			<Transition key={location.key}>{children}</Transition>
-		</PoseGroup>
+		<ContentWrapper>
+			<PoseGroup>
+				<Transition key={location.key}>{children}</Transition>
+			</PoseGroup>
+		</ContentWrapper>
 	</Wrapper>
 );
 
